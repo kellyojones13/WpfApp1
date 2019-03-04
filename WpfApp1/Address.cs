85133223fdecec8b8b2dd182bebc73f6fab70c03
Kelly Jones <kellyojones@ou.edu>
2019-03-04 12:04:03 -0600

    Hmwk3

---
+++ WpfApp1/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Address
+    {
+        public int StreetNumber { get; set; }
+
+        public string StreetName { get; set; }
+
+        public string State { get; set; }
+
+        public string City { get; set; }
+
+        public int ZipCode { get; set; }
+    }
+}
