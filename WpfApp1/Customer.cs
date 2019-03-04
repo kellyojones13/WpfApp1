4c8a468a30e5978151327367c9368f8396d6575c
Kelly Jones <kellyojones@ou.edu>
2019-03-04 12:31:04 -0600

    afternoon

---
+++ WpfApp1/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Customer
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Address Address { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+
+        public Customer()
+        {
+            FirstName = "";
+            LastName = "";
+            Address = ;
+            BirthDate = ;
+
+        }
+
+        public Customer (string firstName, string LastName, Address address, DateTime birthDate)
+
+        
+       
+            
+         public int CalculateAge
+        {
+
+        }
+
+    }
+
+}
